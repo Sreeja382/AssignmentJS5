aac5601d0d3f88d81bc9598566afc3071b222dbb
Sreeja382 <mothukurusreeja@gmail.com>
2021-11-30 19:40:58 +0530

    initial commit

--- assignments/play_with_array/play_with_array.js
+++ assignments/play_with_array/play_with_array.js
@@ -7,7 +7,15 @@ function getEven(arr) {
 
         Write you code below
     */
-
+    let new_arr=[]
+    for(var i=0; i<arr.length;i=i+1){
+        if(arr[i]%2===0){
+            new_arr.push(arr[i])
+        }
+        
+    }
+    
+    return new_arr
 }
 
 function multiplyByN(arr, n) {
@@ -15,9 +23,14 @@ function multiplyByN(arr, n) {
         Return an array whose elements are multiplied by n
         Example
             Input: [1,3,4,55], 3
-            Output: [3,9,13,165]
+            Output: [3,9,12,165]
         Write you code below
     */
+    let new_arr=[]
+    for(var i=0; i<arr.length; i++){
+        new_arr.push(arr[i]*n)
+    }
+    return new_arr
 }
 
 function removeNthElement(arr, n) {
@@ -28,6 +41,13 @@ function removeNthElement(arr, n) {
             Output: [1,3,4,7]
         Write you code below
     */
+    new_arr=[]
+    for(i=0;i<arr.length;i++){
+        if(i!==n){
+            new_arr.push(arr[i])
+        }
+    }
+    return new_arr
 }
 
 module.exports = {
